646790b5d15cee339cd3eb805d163869cc0d4e1f
renyuhuiharrison <renyuhuiharrison@gmail.com>
2022-02-08 11:52:20 +0800

    新增assimp库的属性配置

--- SimpleQtOpenGLApp/OpenGLWidget.cpp
+++ SimpleQtOpenGLApp/OpenGLWidget.cpp
@@ -163,7 +163,6 @@ void OpenGLWidget::paintGL()
 		m_shaderProgram.setUniformValue("projMatrix", projMatrix);
 
 		QOpenGLVertexArrayObject::Binder vaoBinder(&m_vao);
-		//glDrawArrays(GL_TRIANGLES, 0, sizeof(tri_vertices) / sizeof(tri_vertices[0]));
 
 #if 0
 		glGenVertexArrays(1, &m_VAO);
@@ -226,32 +225,32 @@ bool OpenGLWidget::createShader()
 void OpenGLWidget::keyPressEvent(QKeyEvent* event)
 {
 	switch (event->key()) {
-	case Qt::Key_W:
+	case Qt::Key_W: //相机视角向前移动
 	{
 		m_camera->move(Camera::FORWARD, m_deltaTime);
 		break;
 	}
-	case Qt::Key_S:
+	case Qt::Key_S: //相机视角向后移动
 	{
 		m_camera->move(Camera::BACKWARD, m_deltaTime);
 		break;
 	}
-	case Qt::Key_D:
+	case Qt::Key_D: //相机视角向右移动
 	{
 		m_camera->move(Camera::LEFT, m_deltaTime);
 		break;
 	}
-	case Qt::Key_A:
+	case Qt::Key_A: //相机视角向左移动
 	{
 		m_camera->move(Camera::RIGHT, m_deltaTime);
 		break;
 	}
-	case Qt::Key_Q:
+	case Qt::Key_Q: //相机视角向上移动
 	{
 		m_camera->move(Camera::UP, m_deltaTime);
 		break;
 	}
-	case Qt::Key_E:
+	case Qt::Key_E: //相机视角向下移动
 	{
 		m_camera->move(Camera::DOWN, m_deltaTime);
 		break;
